4a9782d506fc431a2d9c5d9dc767e199fb1a48a4
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-05-14 19:22:28 -0300

    primera etapa

---
+++ 2dapreentrega/main.js
@@ -0,0 +1,34 @@
+/*for(i=0;i<2;i++){
+let a =prompt("ingrese un producto acuatico")
+let b =prompt("ingrese un accesorio para un deporte acuatico")
+let c =prompt("ingrese una indumentaria para deporte acuatico")
+
+const surf = [
+    {id:1, nombre: a, precio: 1000},
+    {id:2, nombre: b, precio: 1200},
+    {id:3, nombre: c, precio: 1500},
+]
+for(const item of surf){
+    alert(item.nombre)
+    alert(item.precio)
+}}*/
+
+
+
+/*const maquinas= []
+for(i=0;i<5;i++){
+    let nombre=prompt("ingrese el nombre de la maquina")
+    let wc=prompt("ingrese el nombre de el workcenter")
+    let etapa=prompt("ingrese el nombre de la etapa del proceso")
+    let objeto={
+        nombre: nombre,
+        workcenter:wc,
+        etapa:etapa,
+    }
+    maquinas.push(objeto)
+    for(const item of maquinas){
+        alert(item.nombre)
+        alert(item.workcenter)
+        alert(item.etapa)
+    }
+}*/
