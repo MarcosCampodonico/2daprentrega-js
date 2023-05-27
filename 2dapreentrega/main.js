f5757245b376fab0d0c1fcc4a2857748ac8bfe77
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-05-27 15:21:48 -0300

    reformas

--- 2dapreentrega/main.js
+++ 2dapreentrega/main.js
@@ -50,8 +50,11 @@ let productoNacional = precio("producto nacional")
 console.log(productoNacional(1000,200))
 let productoImportado = precio("producto importado")
 console.log(productoImportado(1000,200))*/
+/*const encontrar = productos.find(item => item.nombre==="tabla")
+console.log(encontrar)*/
 
 // recorrida de arreglos con metodos avanzados
+//este va a ser el arreglo que se va a utilizar como base.
 const productos = [
     {id: 1, nombre: "tabla", precio: 2000},
     {id: 2, nombre: "traje", precio: 1400},
@@ -59,22 +62,44 @@ const productos = [
     {id: 4, nombre: "pita",  precio: 1800},
 ]
 
-/*productos.forEach(item=>{
+productos.forEach(item=>{
     alert(item.id)
     alert(item.nombre)
     alert(item.precio)
-})*/
-
-/*const encontrar = productos.find(item => item.nombre==="tabla")
-console.log(encontrar)*/
+})
 
 //ahora vamos a simular que estamos en la etapa del hot sale y por ende vamos a modificar el arreglo.
-/*const hotSale = productos.map(item =>{
+const hotSale = productos.map(item =>{
     return{
         id: item.id,
-        nombre: item.nombre + "hotsale",
+        nombre: item.nombre +" " + "hotsale",
         precio: item.precio * 0.80
     }
 })
-console.log(hotSale)*/
+console.log(hotSale)
+//ahora vamos a aplicar una funcion con un parametro y de acorde al tipo de bien que sea va realizar un descuento o un aumento.
+function precio(pr){
+    if(pr = "producto nacional"){
+        return(a,b) => a*b
+    }
+    if(pr ="producto importado"){
+        return(a,b) => a*b
+    }
+}
+let cantidad = parseInt(prompt("ingrese la cantidad de productos que lleva"))
+for(i=0;i<cantidad;i++){
+let z =prompt("ingrese que tipo de producto es")
 
+if(z ==="producto nacional"){
+    var p =parseInt(prompt("ingrese el precio del producto"))
+    var d = parseFloat(prompt("ingrese el descuento a aplicar"))
+}
+if(z ==="producto importado"){
+    var p =parseInt(prompt("ingrese el precio del producto"))
+    var d = parseFloat(prompt("ingrese el impuesto a aplicar"))
+}
+    
+
+let producto1 = precio(z);
+alert(producto1(p,d));
+}
