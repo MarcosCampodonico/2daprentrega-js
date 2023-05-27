6460cf552efde27618f1c85ae8d232f29e0ffbeb
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-05-27 15:23:29 -0300

    reformas v2

---
+++ 2dapreentrega/practica.js
@@ -0,0 +1,56 @@
+/*for(i=0;i<2;i++){
+let a =prompt("ingrese un producto acuatico")
+let b =prompt("ingrese un accesorio para un deporte acuatico")
+let c =prompt("ingrese una indumentaria para deporte acuatico")
+
+const surf = [
+    {id:1, nombre: a, precio: 1000},
+    {id:2, nombre: b, precio: 1200},
+    {id:3, nombre: c, precio: 1500},
+]
+for(const item of surf){
+    alert(item.nombre)
+    alert(item.precio)
+}}*/
+
+
+
+/*const maquinas= []
+for(i=0;i<5;i++){
+    let nombre=prompt("ingrese el nombre de la maquina")
+    let wc=prompt("ingrese el nombre de el workcenter")
+    let etapa=prompt("ingrese el nombre de la etapa del proceso")
+    let objeto={
+        nombre: nombre,
+        workcenter:wc,
+        etapa:etapa,
+    }
+    maquinas.push(objeto)
+    for(const item of maquinas){
+        alert(item.nombre)
+        alert(item.workcenter)
+        alert(item.etapa)
+    }
+}*/
+
+
+
+
+
+/*function precio(x){//funcion con un parametro, que una vez aplicado ese parametro me puede returnar otra funcion
+    if(x === "producto nacional"){
+        return(a,b)=> a - b
+    }
+    if(x ==="producto importado"){
+        return(a,b)=>a + b
+    }
+
+}
+let productoNacional = precio("producto nacional")
+console.log(productoNacional(1000,200))
+let productoImportado = precio("producto importado")
+console.log(productoImportado(1000,200))*/
+/*const encontrar = productos.find(item => item.nombre==="tabla")
+console.log(encontrar)*/
+
+// recorrida de arreglos con metodos avanzados
